ad6e7ce6bf5fc4da7998f01330b6d7d1a227afc1
bjornakr <bjornakr@gmail.com>
2016-03-17 14:14:49 +0100

    Refactored parseActivities. Uses Reponse instead of csv string as input.

--- cbcl2ahk.hs
+++ cbcl2ahk.hs
@@ -12,27 +12,22 @@ toResponse :: Char -> Response
 toResponse ' ' = Nothing
 toResponse c = Just ((read (c:[]))::Int)
 
-
 toResponses :: String -> [Response]
 toResponses [] = []
 toResponses ";" = []
+toResponses (';':xs) = Nothing:(toResponses xs)
 toResponses (x:[]) = (toResponse x):[]
 toResponses (x:';':xs) = (toResponse x):(toResponses xs)
+toResponses (x:xs) = error $ "Error trying to parse: " ++ (x:xs)
 
-parseSports :: Int -> String -> [Response]
-parseSports 0 xs = []
-parseSports n (x:';':y:';':xs) = (toResponses (x:';':y:';':[])) ++ (parseSports (n-1) xs)
-
-parseJob :: Int -> String -> [Response]
-parseJob 0 xs = []
-parseJob n (x:';':xs) = (toResponse x):(parseJob (n-1) xs)
-
-parseActivities :: (Int -> String -> [Response]) -> String -> [Response]
-parseActivities parse (noOfJobs:';':xs) = (toResponse noOfJobs):(parse ((read (noOfJobs:[]))::Int) xs)
+parseActivities :: Int -> [Response] -> [Response]
+parseActivities n ((Just noOfJobs):xs) = (Just noOfJobs):(take (n*noOfJobs) xs)
+parseActivities n (Nothing:xs) = [Nothing]
 parseActivities _ _ = error "Error parsing sports/jobs"
 
-
 main = do
-    let a = toResponses "0;1;2; ;0;1;2; ;0;1;2"
-    let b = parseActivities parseJob "3;1;2;3;4;5;6;9;9;"
+    let a = toResponses "0;1;2;;0;1;2; ;0;1;2;0;1;2;3;1;2;3;4;1;2;3;1;2;3;1;4;2;3;2"
+    let b = parseActivities 2 (toResponses "3;;2;1;2;3;4;5;6;9;9;")
+    putStrLn $ toKeyString a
     putStrLn $ toKeyString b
+    putStrLn $ createScript $ toKeyString a
